254f1762f760ef05060a4942b64c873aff9007e6
williansilvap25 <72638419+williansilvap25@users.noreply.github.com>
2020-10-10 00:59:34 -0300

    Trabalho finalizado

---
+++ Models/PacienteDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace apiPacientes.Models
+{
+    public class PacienteDbContext : DbContext
+    {
+        public PacienteDbContext(DbContextOptions<PacienteDbContext> options): base(options){}
+
+        public DbSet<Paciente> Pacientes {get;set;}
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using apiPacientes.Models;
+using apiPacientes.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace apiPacientes
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var connection = Configuration["ConexaoMySql:MySqlConnectionString"];
+            services.AddDbContext<PacienteDbContext>(options =>
+                options.UseMySql(connection)
+            );
+            //setar os repositórios
+            services.AddTransient<IPacienteRepository, PacienteRepository>();
+            services.AddControllers();
+            services.AddCors(
+                options => options.AddDefaultPolicy(
+                    builder => builder
+                                .AllowAnyOrigin()
+                                .AllowAnyMethod()
+                                .AllowAnyHeader()
+                )
+            );
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Não usar para aceitar sem comunicação SSL
+            //app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            
+        }
+    }
+}
